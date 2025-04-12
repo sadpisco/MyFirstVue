999a2f0a4dc245f8b1c14451167496e9772456ec
sadpisco <leo99153@gmail.com>
2025-04-11 20:56:11 -0500

    Playground changes

--- src/components/atoms/ActualForm/ActualForm.vue
+++ src/components/atoms/ActualForm/ActualForm.vue
@@ -2,7 +2,9 @@
   <section
     class="rounded-xl p-4 w-72 flex flex-col justify-center gap-4 shadow-2xl shadow-black bg-slate-200 text-black"
   >
-    <h1 class="font-bold">Actual Form</h1>
+    <form>
+      <h1 class="font-bold">Actual Form</h1>
+    </form>
   </section>
 </template>
 <script setup></script>
